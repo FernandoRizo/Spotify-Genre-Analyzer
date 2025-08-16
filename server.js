3dc3a1bd2a90e1b00a197f0ce672966c015c7a23
Fernando Rizo <oscar.rizo9219@alumnos.udg.mx>
2025-08-15 21:19:56 -0600

    Actualización de Middleware

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 // --- RUTAS DE AUTENTICACIÓN ---
 
